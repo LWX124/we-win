c52fbe1f412c94d677b7e3c19924023f7913f4ba
lwx <liufeng124.lwx@gmail.com>
2026-04-26 22:52:14 +0800

    feat: add history page with multi-fund comparison chart

--- lib/trpc/router/index.ts
+++ lib/trpc/router/index.ts
@@ -1,10 +1,12 @@
 import { router } from "../init";
 import { fundRouter } from "./fund";
 import { arbitrageRouter } from "./arbitrage";
+import { historyRouter } from "./history";
 
 export const appRouter = router({
   fund: fundRouter,
   arbitrage: arbitrageRouter,
+  history: historyRouter,
 });
 
 export type AppRouter = typeof appRouter;
